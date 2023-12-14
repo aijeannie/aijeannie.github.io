235689d2c93bd7f29a6e8bb43c60732974a526aa
thirunavukkarasu <thirunavukkarasu.sam@gmail.com>
2023-12-14 18:54:23 +0530

    added github actions workflow and removed build folder

--- build/assets/js/5e95c892.08098838.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkai_jeannie_docs=self.webpackChunkai_jeannie_docs||[]).push([[9661],{1892:(e,s,a)=>{a.r(s),a.d(s,{default:()=>t});a(7294);var n=a(512),r=a(1944),c=a(5281),u=a(8790),i=a(6040),d=a(5893);function t(e){return(0,d.jsx)(r.FG,{className:(0,n.Z)(c.k.wrapper.docsPages),children:(0,d.jsx)(i.Z,{children:(0,u.H)(e.route.routes)})})}}}]);
--- build/assets/js/e16015ca.1863dfa5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkai_jeannie_docs=self.webpackChunkai_jeannie_docs||[]).push([[9700],{5688:a=>{a.exports=JSON.parse('{"label":"hola","permalink":"/blog/tags/hola","allTagsPath":"/blog/tags","count":1,"unlisted":false}')}}]);

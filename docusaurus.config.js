09550e20ce68a25ae6da0573d3e88330e3e55ac5
Lakshman Peethani <lakshman_peethani@epam.com>
2023-12-18 17:31:34 +0530

    minor fixes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "AI Jeannie",
-  tagline: "Atlassian JIRA Assistant for Agile Teams",
+  tagline: "Atlassian JIRA AI Plugin for Agile Teams",
   favicon: "img/logo.png",
 
   // Set the production url of your site here

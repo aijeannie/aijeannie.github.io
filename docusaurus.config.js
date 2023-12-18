2d9220d1ea6405527ecc2450c26ab46d6e93cca0
thirunavukkarasu <thirunavukkarasu.sam@gmail.com>
2023-12-18 16:36:32 +0530

    fixed social card into jpg image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/aijeannie-social-card.png",
+      image: "img/aijeannie-social-card.jpg",
       navbar: {
         title: "AI Jeannie",
         logo: {
